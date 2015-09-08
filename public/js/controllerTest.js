deeddf87eb8f098588db4c9d4c7bcfa483005f23
Tarek S Hafez <thafez@ymail.com>
2015-09-08 14:43:24 -0500

    Created controller-test.html in public directory and public/js/controllerTest.js . Created controller properties for the shopping cart to interpret data from itemsForm form and evaluate the data entered to calculate totals for line items, subtotals for all items entered, taxes on items entered, shipping cost to items entered, and the total cost of all items with taxes and shipping included. Added a remove button for items to be removed. Added validation for the form inputs and css styling to the entire input groups and display.
    Updated models-test.html to properly format dates

---
+++ public/js/controllerTest.js
@@ -0,0 +1,70 @@
+(function () {
+    'use strict';
+
+    var app = angular.module("CartTest", []);
+
+    app.controller('CartController', function () {
+        this.items = [];
+
+        this.newItem = {
+            name:     "",
+            quantity: "1",
+            price:    ""
+        };
+
+        this.addItem = function (itemsForm) {
+            this.items.push(this.newItem);
+
+            this.newItem = {
+                name:     "",
+                quantity: "1",
+                price:    ""
+            };
+
+            itemsForm.$setPristine();
+            itemsForm.$setUntouched();
+        };
+
+        this.getSubtotal = function () {
+            var subtotal = 0;
+            
+            for (var i = 0; i < this.items.length; i += 1) {
+                subtotal += this.items[i].quantity * this.items[i].price;
+            }
+
+            return subtotal;
+        };
+
+        this.getTaxes = function () {
+            var taxes = .08125;
+
+            var price = this.getSubtotal();
+
+            var setTax = price * taxes;
+
+            return setTax;
+        };
+
+        this.getShipping = function () {
+            var shipping = 1.25;
+            var totalShipping = 0;
+            for (var i = 0; i < this.items.length; i += 1) {
+                totalShipping += shipping * this.items[i].quantity;
+            }
+
+            return totalShipping;
+        };
+
+        this.getTotal = function () {
+            var total = 0;
+
+            total = this.getSubtotal() + this.getTaxes() + this.getShipping();
+
+            return total;
+        };
+
+        this.destroyItem = function (index) {
+            this.items.splice(index, 1);
+        };
+    });
+})();
